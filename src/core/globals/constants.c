895d3ba18b60386c2bb3c4561343b6bbb230cd60
eito-fis <eamon@itofisher.com>
2022-02-24 20:58:11 -0500

    ADD: Import constants

--- src/core/globals/constants.c
+++ src/core/globals/constants.c
@@ -1,9 +1,9 @@
 // # include mbedtls.h
 #include <string.h>
-//#include <constants.h>
-// TODO: Actually implelement. 
-int hash_sha256(unsigned char * output_hash, char * input_data, unsigned int input_sz){
+#include <constants.h>
 
+// TODO: Actually implelement.
+int hash_sha256(unsigned char * output_hash, char * input_data, unsigned int input_sz){
     memcpy(output_hash, input_data, TX_HASH_LEN);
     return 1;
 }
