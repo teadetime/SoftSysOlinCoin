02f11f2072e37629da8a0fd000d6233dcf6f55df
Nathan Faber <ntfaber@gmail.com>
2022-03-02 22:03:55 -0500

    Hashing function implemented and comments

--- src/includes/globals/constants.h
+++ src/includes/globals/constants.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <stddef.h>
+
 #define TX_HASH_LEN 32
 #define BLOCK_HASH_LEN 32
+
 
  /*
  returns 1 if successful hash of input data
@@ -10,4 +13,17 @@
  input_sz: number of bytes in input
  output_hash: unsigned * of size TX_HASH_LEN
  */
-int hash_sha256(unsigned char * output_hash, unsigned char * input_data, unsigned int input_sz); 
+int hash_sha256(unsigned char *output_hash, unsigned char *input_data, size_t input_sz); 
+
+
+/*
+Prints out a buffer on len size using hex encoding (twice as many as chars)
+
+title: string title for buffer
+buf: buffer to be printed
+len: size of buffer in bytes
+
+EXample Code:
+dump_buf( "Hash: ", output_hash, TX_HASH_LEN );
+*/
+void dump_buf( const char *title, unsigned char *buf, size_t len );
