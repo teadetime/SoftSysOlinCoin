e8360d70e89adc299fc3c9ad6638a3195d36f4a3
Nathan Faber <ntfaber@gmail.com>
2022-03-04 15:09:37 -0500

    also print a blockchain!

--- src/includes/globals/blockchain.h
+++ src/includes/globals/blockchain.h
@@ -48,3 +48,17 @@ Block *blockchain_find(unsigned char *header_hash);
  * header_hash: Buffer of length BLOCK_HASH_LEN, hash of block header
  */
 BlockChain *blockchain_find_node(unsigned char *header_hash);
+
+/*
+Prints a single hashmap entry to stdout
+
+prefix: string to put in front of all print commands used for tabbing structure
+*/
+void print_blockchain(BlockChain *blockchain_node, char *prefix);
+
+/*
+Prints the entire blockchain Hashmap to stdout
+
+prefix: string to put in front of all print commands used for tabbing structure
+*/
+void print_blockchain_hashmap(char *prefix);
