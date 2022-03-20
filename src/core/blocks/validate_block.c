0d7b3b59b67896070abf853c695754a6b5a12144
Nathan Faber <ntfaber@gmail.com>
2022-03-20 13:30:13 -0400

    validating block implementation and header

--- src/core/blocks/validate_block.c
+++ src/core/blocks/validate_block.c
@@ -1,110 +1,169 @@
+/**
+ * @file validate_block.c
+ * @author Nathan Faber nfaber@olin.edu
+ * @brief implementation of functions used to validate a new block that is 
+ * broadcast on the blockchain and update the various appropriate structures
+ * @version 0.1
+ * @date 2022-03-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "create_block.h"
 #include "mempool.h"
 #include "blockchain.h"
 #include "utxo_pool.h"
-#include "validate_block.h"
-/*
-How do we store branches or do we not worry right now, 
-seems like a list would be okay but it's really a tree data structure?
-*/
+#include "wallet_pool.h"
 
-
-// WILL BE WRITTEN IN A SEPARATE SPOT
+// WILL BE WRITTEN IN A SEPARATE PR by EAMON?
 int validate_tx(Transaction *tx){
+  if(tx){}
   return 0;
 }
-int validate_coinbase_tx(Transaction *tx){
+
+int validate_coinbase_tx(Transaction *coinbase_tx, Transaction **txs, unsigned int num_txs){
+  unsigned int calculated_total_fees = 0;
+  // Note starting at 1 skips the coinbase tx
+  for(unsigned int i = 1; i < num_txs; i++){
+    calculated_total_fees += calc_tx_fees(txs[i]);
+  }
+  if(coinbase_tx->num_inputs != 0){
+    return 1;
+  }
+  if(coinbase_tx->num_outputs != 1){
+    return 2;
+  }
+  if(coinbase_tx->outputs == NULL){
+    return 3;
+  }
+  if(calculated_total_fees + calc_block_reward(chain_height) != coinbase_tx->outputs[0].amt){
+    return 4;
+  }
+
   return 0;
+}
+
+int validate_txs(Transaction **txs, unsigned int num_txs){
+  if(validate_coinbase_tx(txs[0], txs, num_txs) != 0){
+    return 1;
+  }
+  for(unsigned int i = 1; i < num_txs; i++){
+    if(validate_tx(txs[i]) != 0){
+      return 2;
+    }
+  }
+  return 0;
+}
+///OPTIONAL
+void request_prev_block(unsigned char *prev_header){
+  if(prev_header){}
+  //Add the previous missiing header hash somewhere
+}
+
+void add_to_pending_blocks(unsigned char *curr_header){
+  if(curr_header){}
+  // Add to the blocks that need to be validated once we have teh previous block
 }
 
 int validate_prev_block_exists(Block *block){
   // Also check if it's in blockchain already....
   // Check if prev block is our latest top block
 
-  int failure = 0; // 0 is successful
   Block *prev_block = blockchain_find(block->header.prev_header_hash);
   if(prev_block == NULL){
-    failure = 1;
-    return 1;
-  }
-
-  if(memcmp(block->header.prev_header_hash, get_prev_header_hash(), BLOCK_HASH_LEN) != 0 ){
-    // Not the top which means we should get more blocks
-    failure = 2;
     add_to_pending_blocks(block->header.prev_header_hash);
     request_prev_block(block->header.prev_header_hash);
     return 1;
   }
 
-  // Check if block already in blockchain
-  
-
-  if(failure != 0){
-    return 1;
+  if(memcmp(block->header.prev_header_hash, top_block_header_hash, BLOCK_HASH_LEN) != 0 ){
+    // NOTE this means we already have atleast one block ahead
+    return 2;
   }
-  else{
-    return 0;
-  }
+  return 0;
 }
 
 int validate_all_tx_hash(Block *block){
-  return 1;
+  unsigned char calculated_all_tx_hash[ALL_TX_HASH_LEN];
+  hash_all_tx(calculated_all_tx_hash, block->txs, block->num_txs);
+  return (memcmp(block->header.all_tx, calculated_all_tx_hash, ALL_TX_HASH_LEN) != 0);
 }
 
-int validate_block_header(Block *block){
+int validate_block(Block *block){
+  int valid_hash = try_header_hash(&(block->header));
+  if(valid_hash != 0){
+    return 1;
+  }
 
-}
+  int valid_prev_block = validate_prev_block_exists(block);
+  if(valid_prev_block != 0){
+    return 2;
+  }
 
-int try_validate_block(Block *new_block){
-  // Check if hash meets the difficulty using other function
-  return mine_block(new_block);
+  int valid_all_tx_hash = validate_all_tx_hash(block);
+  if(valid_all_tx_hash != 0){
+    return 3;
+  }
+
+  int valid_txs = validate_txs(block->txs, block->num_txs);
+  if(valid_txs != 0){
+    return 4;
+  }
+
+  return 0;
 }
 
 void update_local_blockchain(Block *block){
-  // Do we need to resolve branches here?
-  //cahin heght
-  //top block hash
-  //add to hashmap
-  chain_height += 1;
+  // NOTE: NOT DEALING WITH BRANCHES HERE
   hash_blockheader(top_block_header_hash, &(block->header));
-  blockchain_add(block);
+  blockchain_add(block);  // T\NOTE this increases chain height and sets new top header hash
 }
 
-void update_UTXO_pool(Block *block){
-  //Create new utxo
-  //Remove old should we free them!?! or just put spent
+void update_UTXO_pool_and_wallet_pool(Block *block){
+  for(unsigned int i = 0 ; i < block->num_txs; i++){
+    for(unsigned int j = 0; j < block->txs[i]->num_outputs; j++){
+      utxo_pool_add(block->txs[i], j); // Don't need to do anything with return
+      mbedtls_ecdsa_context *keypair = check_if_output_unlockable(block->txs[i], j);
+      if(keypair != NULL){
+        wallet_pool_add(block->txs[i], j, keypair);
+      }
+    }
+    for(unsigned int k = 0; k < block->txs[i]->num_inputs; k++){
+      utxo_pool_spend(block->txs[i]->inputs[k].prev_tx_id,
+        block->txs[i]->inputs[k].prev_utxo_output);
+
+      wallet_pool_remove(block->txs[i]->inputs[k].prev_tx_id, 
+        block->txs[i]->inputs[k].prev_utxo_output);
+    }
+  }
 }
 
 void update_mempool(Block *block){
   //Remove txs from mempool
-}
-
-void update_wallet_pool(){
-  // If utxo have been spent those should be removed
-  //Does anyhting happent othe keypool
+  unsigned char tx_hash_to_remove[TX_HASH_LEN];
+  for(unsigned int i = 0; i < block->num_txs; i++){
+    hash_tx(tx_hash_to_remove, block->txs[i]);
+    Transaction *ret_tx = mempool_remove(tx_hash_to_remove);
+    if(ret_tx == NULL){
+      // This would be bad
+    }
+  }
 }
 
 void accept_block(Block *block){
   // ALl the thigns to do when the block is good
   update_local_blockchain(block);
-  update_UTXO_pool(block);
+  update_UTXO_pool_and_wallet_pool(block);
   update_mempool(block);
 }
 
 void handle_new_block(Block *block){
-  int ret = try_validate_block(block);
+  int ret = validate_block(block);
   if(ret == 0){
     accept_block(block);
   }
   else{
+    // Different messages here to determine if we need to request headers etc
+    //Right now we are just accepting the block or rejecting, no in between
   }
-  //Wrapper for all behaviour witha new block
-}
-///OPTIONAL
-void request_prev_block(unsigned char *prev_header){
-  //Add the previous missiing header hash somewhere
-}
-
-void add_to_pending_blocks(unsigned char *prev_header){
-  // Add to the blocks that need to be validated once we have teh previous block
 }
--- src/includes/blocks/validate_block.h
+++ src/includes/blocks/validate_block.h
@@ -1,7 +1,8 @@
 /**
 * @file validate_block.h
 * @author Nathan Faber (nfaber@olin.edu.com)
-* @brief HEader for functions to handle block validation when a block is received
+* @brief Header for fof functions used to validate a new block that is 
+ * broadcast on the blockchain and update the various appropriate structures
 * @version 0.1
 * @date 2022-03-18
 * 
@@ -10,8 +11,34 @@
 #pragma once
 #include "base_tx.h"
 #include "base_block.h"
+/*
+How do we store branches or do we not worry right now, 
+seems like a list would be okay but it's really a tree data structure?
+*/
 
-/* how should we maintain the set of blocks to validate?
- Seems like a hashmap would be good so we can check the header hash, 
- but also keep order if we are requesting blocks*/
 
+// WILL BE WRITTEN IN A SEPARATE PR by EAMON?
+int validate_tx(Transaction *tx);
+
+int validate_coinbase_tx(Transaction *coinbase_tx, Transaction **txs, unsigned int num_txs);
+
+int validate_txs(Transaction **txs, unsigned int num_txs);
+///OPTIONAL
+void request_prev_block(unsigned char *prev_header);
+
+void add_to_pending_blocks(unsigned char *curr_header);
+
+int validate_prev_block_exists(Block *block);
+
+int validate_all_tx_hash(Block *block);
+int validate_block(Block *block);
+
+void update_local_blockchain(Block *block);
+
+void update_UTXO_pool_and_wallet_pool(Block *block);
+
+void update_mempool(Block *block);
+
+void accept_block(Block *block);
+
+void handle_new_block(Block *block);
