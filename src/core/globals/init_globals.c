4e3614a48a4f2a137aa52410f5ae92fd5033565b
Nathan Faber <ntfaber@gmail.com>
2022-04-25 14:08:07 -0400

    concurrent miner and block processor

--- src/core/globals/init_globals.c
+++ src/core/globals/init_globals.c
@@ -4,11 +4,14 @@
 #include "utxo_pool.h"
 #include "mempool.h"
 #include "init_db.h"
+#include "wallet_pool.h"
 
 void node_init(char *db_env) {
   // Read from file for chain height and top block hash
   blockchain_init_leveldb(db_env);
   utxo_pool_init_leveldb(db_env);
+  wallet_init_leveldb(db_env);
+  mempool_init();
 }
 
 void miner_init() {
--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -5,6 +5,9 @@
 #include "handle_block.h"
 #include "create_block.h"
 
+#include "init_globals.h"
+#include "init_db.h"
+
 typedef struct Globals {
   Queue *queue_block;
   Queue *queue_tx;
@@ -46,34 +49,14 @@ Globals *init_globals(){
   return new_globals;
 }
 
-void *add_and_delay(void *queue){
-  Queue *thread_queue = (Queue *) queue;
-  int *test1 = malloc(sizeof(int));
-  *test1 = 5;
-  printf("Starting thread 1\n");
-  queue_add_void(thread_queue, test1);
-  printf("Done Adding thread 1\n");
-
-  printf("Add and Delay Returns ...\n");
-  return NULL;
-}
-
-void *pop(void *queue){
-  Queue *thread_queue = (Queue *) queue;
-
-  printf("Starting thread pop\n");
-  int *ret = queue_pop_void(thread_queue);
-  printf("Done pop thread 2, popped: %i\n", *ret);
-  printf("Add Returns ...\n");
-  return NULL;
-}
-
 void *node_block_thread(void *arg){
   Globals *globals = arg;
   while(1){
+    printf("Node Thread Waiting to pop Block\n");
     Block *popped_block = queue_pop_void(globals->queue_block);
-
+    printf("Node Thread Block Popped\n");
     //Now aquire locks for validation (Utxo pool and blockcahin)
+    printf("Node Thread Waiting on lock for validation\n");
     pthread_mutex_lock(&globals->utxo_pool_lock);
     pthread_mutex_lock(&globals->blockchain_lock);
     int block_valid = validate_block(popped_block);
@@ -81,6 +64,7 @@ void *node_block_thread(void *arg){
 
     if(block_valid == 0){
       //Now aquire additional locks for handling
+      printf("Node Thread Waiting on lock for Handling\n");
       pthread_mutex_lock(&globals->utxo_to_tx_lock);
       pthread_mutex_lock(&globals->wallet_pool_lock);
       pthread_mutex_lock(&globals->key_pool_lock);
@@ -91,6 +75,7 @@ void *node_block_thread(void *arg){
       pthread_mutex_unlock(&globals->wallet_pool_lock);
       pthread_mutex_unlock(&globals->key_pool_lock);
       pthread_mutex_unlock(&globals->mempool_lock);
+      printf("Node Thread unlockedr Handling\n");
     }
     pthread_mutex_unlock(&globals->utxo_pool_lock);
     pthread_mutex_unlock(&globals->blockchain_lock);
@@ -100,25 +85,29 @@ void *node_block_thread(void *arg){
 
 void *miner_thread(void *arg){
   Globals *globals = arg;
+  while(1){
+    printf("Miner Thread waiting on lock to create Block\n");
+    //Now aquire locks for creating a block!
+    pthread_mutex_lock(&globals->utxo_pool_lock);
+    pthread_mutex_lock(&globals->blockchain_lock);
+    pthread_mutex_lock(&globals->key_pool_lock) ;
+    pthread_mutex_lock(&globals->mempool_lock);
+    printf("Miner Thread got lock to create Block\n");
+    Block *new_block = create_block_alloc();
 
-  //Now aquire locks for creating a block!
-  pthread_mutex_lock(&globals->utxo_pool_lock);
-  pthread_mutex_lock(&globals->blockchain_lock);
-  pthread_mutex_lock(&globals->key_pool_lock) ;
-  pthread_mutex_lock(&globals->mempool_lock);
-  
-  Block *new_block = create_block_alloc();
+    pthread_mutex_unlock(&globals->utxo_pool_lock);
+    pthread_mutex_unlock(&globals->blockchain_lock);
+    pthread_mutex_unlock(&globals->key_pool_lock) ;
+    pthread_mutex_unlock(&globals->mempool_lock);
+    printf("Miner Thread done creating Block\n");
 
-  pthread_mutex_unlock(&globals->utxo_pool_lock);
-  pthread_mutex_unlock(&globals->blockchain_lock);
-  pthread_mutex_unlock(&globals->key_pool_lock) ;
-  pthread_mutex_unlock(&globals->mempool_lock);
-  
-  while(try_header_hash(&(new_block->header)) != 0){
-    change_nonce(new_block);
+    while(try_header_hash(&(new_block->header)) != 0){
+      change_nonce(new_block);
+    }
+
+    printf("Miner mined a block Block\n");
+    queue_add_void(globals->queue_block, new_block);
   }
-
-  queue_add_void(globals->queue_block, new_block);
   return NULL;
 }
 
@@ -126,29 +115,29 @@ int main() {
 
   Globals *globals = init_globals();
 
-  int *test1 = malloc(sizeof(int));
-  *test1 = 4;
-  queue_add_void(globals->queue_block, test1);
-  int *pop_int = queue_pop_void(globals->queue_block);
-  printf("Popped val: %i\n", *pop_int);
+  // Intitialize the globabls!
+  node_init(PROD_DB_LOC);
 
   pthread_t node_block, node_tx, shell, miner;
   int node_block_ret, node_tx_ret, shell_ret, miner_ret;
   /* Create independent threads each of which will execute function */
 
-  node_block_ret = pthread_create( &node_block, NULL, pop, (void*) globals->queue_block);
-  sleep(1);
-  node_tx_ret = pthread_create( &node_tx, NULL, add_and_delay, (void*) globals->queue_block);
+  // node_block_ret = pthread_create( &node_block, NULL, pop, (void*) globals->queue_block);
+  // sleep(1);
+  // node_tx_ret = pthread_create( &node_tx, NULL, add_and_delay, (void*) globals->queue_block);
   
-
+  node_block_ret = pthread_create( &node_block, NULL, node_block_thread, (void*) globals);
+  sleep(1);
+  miner_ret = pthread_create( &miner, NULL, miner_thread, (void*) globals);
+  
   /* Wait till threads are complete before main continues. Unless we  */
   /* wait we run the risk of executing an exit which will terminate   */
   /* the process and all threads before the threads have completed.   */
 
   pthread_join(node_block, NULL);
-  pthread_join(node_tx, NULL); 
+  pthread_join(miner, NULL); 
 
   printf("Thread 1 returns: %d\n",node_block_ret);
-  printf("Thread 2 returns: %d\n", node_tx_ret);
+  printf("Thread 2 returns: %d\n", miner_ret);
   return 0;
 }
